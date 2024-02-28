34297f54c4f105c1f24d0951501438ccb0c2a0c7
Djordje Mihajlovic <djdjdjordje@gmail.com>
2024-02-28 10:24:16 +0000

    check if 6 varies

--- src/knotbio/vassiliev.py
+++ src/knotbio/vassiliev.py
@@ -16,7 +16,7 @@ def load(knot_type, Nbeads, pers_len):
     fname_sts = f"SIGWRITHEMATRIX/3DSignedWritheMatrix_{knot_type}.dat.lp10.dat"
     my_knot_dir = "/Users/djordjemihajlovic/Desktop/Theoretical Physics/MPhys/Data"
     fname_sts = f"SIGWRITHEMATRIX/3DSignedWritheMatrix_{knot_type}.dat.lp10.dat"
-    STS = np.loadtxt(os.path.join(my_knot_dir, fname_sts))
+    STS = np.loadtxt(os.path.join(master_knots_dir, fname_sts))
     STS = STS.reshape(-1, Nbeads, Nbeads)
     return STS
 
@@ -31,8 +31,8 @@ def vassiliev_combinatorical(STS, test_points):
     '''
     Calculate the Vassiliev invariants for a given knot
     '''
-    samples = 1
-    combinatorics = 4
+    samples = 3
+    combinatorics = 6
     vassiliev_data = []
     for idy in range(0, samples): # samples
         integral = 0
@@ -43,6 +43,7 @@ def vassiliev_combinatorical(STS, test_points):
                 integral_alt += STS[idy][i[0], i[3]]*STS[idy][i[1], i[3]]*STS[idy][i[2], i[3]]
             elif combinatorics == 6:
                 integral += STS[idy][i[0], i[3]]*STS[idy][i[1], i[4]]*STS[idy][i[2], i[5]]
+                                                            
             elif combinatorics == 8:
                 integral += STS[idy][i[0], i[4]]*STS[idy][i[1], i[5]]*STS[idy][i[2], i[6]]*STS[idy][i[3], i[7]]
 
@@ -53,10 +54,34 @@ def vassiliev_combinatorical(STS, test_points):
 
     return avg_vassiliev, vassiliev_data
 
-knots = ["3_1"]
+@njit
+def milnor_combinatorical(STS, test_points):
+    '''
+    Calculate the Milnor invariants for a given knot
+    '''
+    samples = 100
+    combinatorics = 3
+    milnor_data = []
+    for idy in range(0, samples):
+        print(idy)
+        integral = 0
+        integral_alt = 0
+        for idx, i in enumerate(test_points):
+            if combinatorics == 3:
+                integral += STS[idy][i[0], i[1]]*STS[idy][i[1], i[2]]
+                integral += STS[idy][i[1], i[2]]*STS[idy][i[0], i[2]]
+                integral += STS[idy][i[0], i[1]]*STS[idy][i[0], i[2]]
+
+        milnor = integral
+        milnor_data.append(milnor)
+    avg_milnor = sum(milnor_data) / len(milnor_data)
+
+    return avg_milnor, milnor_data
+
+knots = ["3_1", "4_1", "5_1"]
 avgs = []
 indicies = np.arange(0, 100, 1)
-c = 4
+c = 6
 for x in knots:
     STS = load(x, 100, 10) # this is quite slow
     test_points = combinations(indicies, c)
@@ -67,7 +92,7 @@ for x in knots:
             writer.writerow([item])
     avgs.append(avg)
 
-print(f"Combinatorics {4}: ", avgs)
+print(f"Combinatorics {6}: ", avgs)
     
 
 
