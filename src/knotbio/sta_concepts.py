65693e3f61a257a2d058aac9bbbee8831d7befe4
Djordje Mihajlovic <djdjdjordje@gmail.com>
2024-03-19 14:17:31 +0000

    sta_concepts

--- src/knotbio/sta_concepts.py
+++ src/knotbio/sta_concepts.py
@@ -6,18 +6,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import AutoMinorLocator
 
-knot = "0_1"
 plot = False
 KnotID = ["7_1", "7_2", "7_3", "7_4", "7_5", "7_6", "7_7"]
 KnotIDtex = [r'$5_{1}$', r'$7_{2}$']
-peak_order_data = [[],[],[],[],[]]
-peak_separations = [[],[],[],[],[]]
-sta_area = [[],[],[],[],[]]
-avg_peak = [[],[]]
+peak_order_data = [[],[],[],[],[],[],[]]
+peak_separations = [[],[],[],[],[], [], [], []]
+sta_area = [[],[],[],[],[], [], [], []]
+avg_peak = [[],[], [], [], [], [], []]
 
 for indx, knot in enumerate(KnotID):
 
-    dirname = "/storage/cmstore04/projects/TAPLabKnotsDatabase/knots_database/"
+    master_knots_dir = "/storage/cmstore04/projects/TAPLabKnotsDatabase/knots_database/"
+    Nbeads = 100
+    pers_len = 10
+    dirname = os.path.join(os.path.join(master_knots_dir,knot,f"N{Nbeads}",f"lp{pers_len}"))
     fname = os.path.join("SIGWRITHE", f"3DSignedWrithe_{knot}.dat.lp{10}.dat.nos")
 
     f = open(os.path.join(dirname, fname), "r")
@@ -44,43 +46,40 @@ for indx, knot in enumerate(KnotID):
 
 
 
-    prominences = np.linspace(0.05, 3.5, 50)
+    prominences = np.linspace(0.75, 0.75, 1)
 
+    # for j in range(0, len(prominences)):
 
-    for j in range(0, len(prominences)):
+    peak_count_data = [[],[],[],[],[],[],[],[]]
 
-        peak_count_data = [[],[],[],[],[],[],[],[]]
+    for i in range(0, len(knot_count)): 
 
-        for i in range(0, len(knot_count)): 
+        indices = np.arange(0, len(sta[i]), 1)
+        area = np.trapz(y=sta[i], x=indices)
+        if knot == "4_1" and i == 0:
+            ideal_4_1 = sta[i]
 
+        peaks, properties = find_peaks(sta[i], prominence=0.75)
+        vals = properties['prominences']
+        prom_sum = np.prod(vals)
+        prominence_order = np.array(vals).argsort().tolist()[::-1] # should be invariant to permutation
+        prominence_order = [k+1 for k in prominence_order]
+        peak_order_data[indx].append(prominence_order)
+        peak_count_data[indx].append([len(prominence_order)])
+        if len(peaks) > 1:
+            sep = np.diff(peaks).tolist()
+            extr = 100-peaks[-1]+peaks[0] # looping back
+            sep.append(extr)
+        else:
+            sep = []
 
-
-            indices = np.arange(0, len(sta[i]), 1)
-            area = np.trapz(y=sta[i], x=indices)
-            if knot == "4_1" and i == 0:
-                ideal_4_1 = sta[i]
-
-            peaks, properties = find_peaks(sta[i], prominence=prominences[j])
-            vals = properties['prominences']
-            prom_sum = np.prod(vals)
-            prominence_order = np.array(vals).argsort().tolist()[::-1] # should be invariant to permutation
-            prominence_order = [k+1 for k in prominence_order]
-            peak_order_data[indx].append(prominence_order)
-            peak_count_data[indx].append([len(prominence_order)])
-            if len(peaks) > 1:
-                sep = np.diff(peaks).tolist()
-                extr = 100-peaks[-1]+peaks[0] # looping back
-                sep.append(extr)
-            else:
-                sep = []
-
-            peak_separations[indx].append(sep)
-            sta_area[indx].append([area])
-        avg_peak[indx].append(np.sum(peak_count_data[indx])/len(peak_count_data[indx]))
+        peak_separations[indx].append(sep)
+        sta_area[indx].append([area])
+    avg_peak[indx].append(np.sum(peak_count_data[indx])/len(peak_count_data[indx]))
 
 
 
-    with open(f'../../knot data/sta concepts/peaks prominence=0.75/peak count/peakcount_{knot}_prom=0.75.csv', 'w', newline='') as f:
+    with open(f'/storage/cmstore02/groups/TAPLab/djordje_mlknots/PyKnot/knot data/sta concepts/peaks prominence=0.75/peak count/peakcount_{knot}_prom=0.75.csv', 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerows(peak_count_data[indx]) 
 
